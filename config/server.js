2dca97f1bc28174e108199addcbad4ed0e928179
Leonid Konchulizov <mr.konchulizov@mail.ru>
2021-12-06 21:42:46 +0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e76f52e074c18929b8fee2591c52543a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '34e9fe8d-5540-4f16-bf7d-321ddcd444e7'
+};
